24165d4d9d9d4fc25972b7961b203c0c29978462
Brendon Fatland <b.fatland@gmail.com>
2019-11-15 13:29:46 -0600

    Major push. Deligated Events to css to todo functionaltiy.

--- app.js
+++ app.js
@@ -1,12 +1,79 @@
 $(function() {
+  // SETUP
+  var $list;
+  $list = $("ul");
+
+  // add to list
   $(".addBtn").click(function() {
-    var li = document.createElement("li"); // creates whatever you need. LI
-    var userInput = document.getElementById("myInput").value; // User input
-    var userTextNode = document.createTextNode(userInput); // pushes input to dom text node
-    li.appendChild(userTextNode);
+    var text = document.getElementById("myInput").value; // User input
+    $list.append(
+      "<li class='listLI'>" +
+        "<select class='form-control DropDownCompletionType' id='CT'><option> </option><option>Complete</option><option>In-Progress</option><option>Waiting For Feedback</option><option>Stuck</option></select>" +
+        " " +
+        text +
+        "<div class='divx'><i class='fas fa-window-close'></i></div>" +
+        "</li>"
+    );
+  });
 
-    var position = document.getElementsByClassName("listUL")[0];
+  // Check item or uncheck item .. Simply add class to item on click.
+  var list = document.querySelector("ul");
+  list.addEventListener(
+    "click",
+    function(ev) {
+      if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+      }
+    },
+    false
+  );
 
-    position.appendChild(li);
+  //On dropdown change css of stuff
+  $(".listUL").on("change", ".DropDownCompletionType", function() {
+    if (this.value === "Complete") {
+      $(this)
+        .parent()
+        .removeClass("Complete InProgress Waiting Stuck checked")
+        .addClass("Complete checked");
+    } else if (this.value === "In-Progress") {
+      $(this)
+        .parent()
+        .removeClass("Complete InProgress Waiting Stuck checked")
+        .addClass("InProgress");
+    } else if (this.value === "Waiting For Feedback") {
+      $(this)
+        .parent()
+        .removeClass("Complete InProgress Waiting Stuck checked")
+        .addClass("Waiting");
+    } else if (this.value === "Stuck") {
+      $(this)
+        .parent()
+        .removeClass("Complete InProgress Waiting Stuck checked")
+        .addClass("Stuck");
+    } else {
+      $(this)
+        .parent()
+        .removeClass("Complete InProgress Waiting Stuck checked");
+    }
+  });
+
+  //Onclick X. remove and insert into lowwer portion.
+  $(".listUL").on("click", ".fa-window-close", function() {
+    // var itemToCopy = $(this).parent();
+    // $(".CompletedContainer").html(itemToCopy);
+    $(this)
+      .parent()
+      .parent()
+      .appendTo(".CompletedContainer");
+    // $(itemToCopy)
+    //   .clone()
+    //   .appendTo("body");
+  });
+
+  $("#myInput").keyup(function(e) {
+    console.log(e.keyCode);
+    if (e.keyCode == 13) {
+      $(".addBtn").click();
+    }
   });
 });
